e1a1cdea7fe78f84a196dcec4ce6cea796f178b6
Richard Hu <36656969+PowerfulRichard@users.noreply.github.com>
2023-01-03 13:42:09 +0800

    Update index.php
    Compatible with PHP8

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
-<?php $counter = intval(file_get_contents("counter.dat")); ?>
-<?php include 'function.php';?>
 <?php
+require('function.php');
+$counter = intval(file_get_contents("counter.dat"));
 header("Content-type: image/JPEG");
 //判断mode使用不同图片
-$mode = $_GET["mode"];
-$pic = $_GET["pic"];
-switch($mode)
-{
+isset($_GET['mode'])?$mode = $_GET['mode']:$mode = NULL;
+isset($_GET['pic'])?$pic = $_GET['pic']:$pic = NULL;
+switch($mode) {
   case "1":$im = imagecreatefromjpeg("xhxh.jpg");break;
   case "2":if($pic=="Chtholly"){$im = imagecreatefromjpeg("qmd_Chtholly.jpg");}else if($pic=="Pipi"){$im = imagecreatefromjpeg("qmd_pipi.jpg");}else {$im = imagecreatefromjpeg("qmd.jpg");}break;
   default:$im = imagecreatefromjpeg("xhxh.jpg");break;
 }
 $ip = $_SERVER["REMOTE_ADDR"];
 $weekarray=array("日","一","二","三","四","五","六"); //先定义一个数组
-$get=$_GET["s"];
-$get=base64_decode(str_replace(" ","+",$get));
-//$wangzhi=$_SERVER['HTTP_REFERER'];这里获取当前网址
+isset($_GET['s'])?$get = $_GET['s']:$get = NULL;
+echo($get);
+$get=base64_decode(str_replace(" ","+",$get ?? ''),true);
+echo($get);
+$wangzhi=$_SERVER['HTTP_REFERER'];//这里获取当前网址
 //here is ip 
-$url="http://ip.taobao.com/service/getIpInfo.php?ip=".$ip; 
-$UserAgent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 3.0.04506; .NET CLR 3.5.21022; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';  
+$url="https://api.asilu.com/ip/?ip=".$ip;
+//$UserAgent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 3.0.04506; .NET CLR 3.5.21022; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';  
 $curl = curl_init(); 
 curl_setopt($curl, CURLOPT_URL, $url); 
 curl_setopt($curl, CURLOPT_HEADER, 0);  
@@ -26,22 +27,23 @@ curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);  
 curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);  
 curl_setopt($curl, CURLOPT_ENCODING, '');  
-curl_setopt($curl, CURLOPT_USERAGENT, $UserAgent);  
+//curl_setopt($curl, CURLOPT_USERAGENT, $UserAgent);  
 curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);  
 $data = curl_exec($curl);
 $data = json_decode($data, true);
-$country = $data['data']['country']; 
-$region = $data['data']['region']; 
-$city = $data['data']['city'];
+$dz = $data['dz'];
+$ip = $data['ip'];
+$wl = $data['wl'];
 //定义颜色
 $black = ImageColorAllocate($im, 0,0,0);//定义黑色的值
 $red = ImageColorAllocate($im, 255,0,0);//红色
 $blue = ImageColorAllocate($im, 0,0,255);
 $font = 'msyh.ttf';//加载字体
 //输出
-imagettftext($im, 16, 0, 10, 40, $red, $font,'欢迎您来自'.$country.'-'.$region.'-'.$city.'的朋友');
+//imagettftext($im, 16, 0, 10, 40, $red, $font,'欢迎您来自'.$country.'-'.$region.'-'.$city.'的朋友');
+imagettftext($im, 16, 0, 10, 40, $red, $font,'欢迎您来自'.$dz.'的朋友');
 imagettftext($im, 16, 0, 10, 72, $red, $font, '今天是'.date('Y年n月j日')."  星期".$weekarray[date("w")]);//当前时间添加到图片
-imagettftext($im, 16, 0, 10, 104, $red, $font,'您的IP是:'.$ip);//ip
+imagettftext($im, 16, 0, 10, 104, $red, $font,'您的IP是:'.$ip."(".$wl.")");//ip
 imagettftext($im, 16, 0, 10, 140, $red, $font,'您使用的是'.$os.'操作系统');
 imagettftext($im, 16, 0, 10, 175, $red, $font,'您使用的是'.$bro.'浏览器');
 imagettftext($im, 14, 0, 10, 200, $black, $font,$get); 
@@ -76,6 +78,4 @@ ImageDestroy($im);
      //fwrite($fpip, $ipold+' '+$ip);
      fclose($fp); 
      //fclose($fpip);
-
-
  ?>
